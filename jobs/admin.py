d665622ab15cda7110ab442323b2c9d1c577094d
ishan-akhouri <ishan.akhouri@gmail.com>
2026-02-03 14:45:06 -0500

    initial job portal setup: models, admin, database configured

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Job, Application
 
-# Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'employer', 'job_type', 'location', 'is_active', 'created_at']
+    list_filter = ['job_type', 'is_active', 'created_at']
+    search_fields = ['title', 'description', 'location']
+    date_hierarchy = 'created_at'
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['job', 'applicant', 'status', 'applied_at']
+    list_filter = ['status', 'applied_at']
+    search_fields = ['job__title', 'applicant__username']
+    date_hierarchy = 'applied_at'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, JobSeekerProfile, EmployerProfile
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'user_type', 'first_name', 'last_name']
+    list_filter = ['user_type', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('user_type', 'phone')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('user_type', 'phone')}),
+    )
+
+@admin.register(JobSeekerProfile)
+class JobSeekerProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'experience_years', 'education']
+    search_fields = ['user__username', 'skills']
+
+@admin.register(EmployerProfile)
+class EmployerProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'company_name', 'website']
+    search_fields = ['company_name', 'user__username']
